72f9423ad18666fedafa73a8ff270c63d1a27f08
Hasan Tfaily <hasantfaily@Hasans-MacBook-Pro.local>
2024-09-13 01:45:48 +0300

    fixing server issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import Map from "../component/map";
+"use client"; // Ensures this code runs only on the client side
+
+import dynamic from "next/dynamic";
+
+// Dynamically import the Map component with { ssr: false } to disable server-side rendering
+const Map = dynamic(() => import("../component/map"), { ssr: false });
 
 export default function Home() {
   return (
-    <main className=" min-h-screen h-[2000px] ">
+    <main className="min-h-screen h-[2000px]">
       <Map />
     </main>
   );
